f3203043f58345fd469bc6eb9e90c76ea55b7fe7
MustaSteve <avineusq@gmail.com>
2023-03-16 12:44:30 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints the size of various types
+ * Return: 0 when successfull
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
