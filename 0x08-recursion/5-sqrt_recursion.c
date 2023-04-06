c5e11f4f541fa0bdd7029b9537d2baa4b0a66e35
MustaSteve <avineusq@gmail.com>
2023-04-05 18:57:44 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _cheksqrt_1 - returns root of a number
+ * @n: test number
+ * @x: base number
+ *
+ * Return: the square root of n
+ */
+int _cheksqrt_1(int n, int x)
+{
+	if (n * n > x)
+		return (-1);
+	else if (n * n == x)
+		return (n);
+	return (_cheksqrt_1(n + 1, x));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to check
+ *
+ * Return: root of n
+ */
+int _sqrt_recursion(int n)
+{
+        if (n == 0)
+                return (n);
+        else if (n == 1)
+		return (n);
+        return (_cheksqrt_1(0, n));
+}
