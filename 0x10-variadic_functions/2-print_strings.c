08f0fa0e6faf327b68fa7e924d733c16eedd561e
MustaSteve <avineusq@gmail.com>
2023-04-20 09:04:16 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,40 +1,39 @@
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints strings
+ * print_strings - function that prints strings
  * @separator: string
- * @n: argc
+ * @n: arg
  * @...: strings
  * Return: void
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list arg;
-	char *str;
-	unsigned int i = 0;
-	
+va_list arg;
+unsigned int i = 0;
+char *str;
 
-	va_start(arg, n);
+va_start(arg, n);
 
-	while (i < n)
-	{
-		if (separator == NULL && i == n - 1)
-		{
-			str = va_arg(arg, char *);
-			
-			printf("%s", (str) ? str : "(nil)");
+while (i < n)
 
-		}
+{
+if (separator == NULL || i == n - 1)
+{
 
-		else
+str = va_arg(arg, char *);
+printf("%s", (str) ? str : "(nil)");
 
-		{
-			str = va_arg(arg, char *);
-			
-			printf("%s%s", (str) ? str : "(nil)", separator);
-		}
-		i++;
-	}
-	putchar('\n');
-	va_end(arg);
+}
+else
+{
+
+str = va_arg(arg, char *);
+printf("%s%s", (str) ? str : "(nil)", separator);
+
+}
+i++;
+}
+putchar('\n');
+va_end(arg);
 }
