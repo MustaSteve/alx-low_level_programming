ec59a48b7f2b3c527b3f367dedc9a4b55ec13f4b
MustaSteve <avineusq@gmail.com>
2023-04-19 18:51:53 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,37 +1,33 @@
 #include "3-calc.h"
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
- * op_add - add two num
- * @a: first number
- * @b: second number
+ * op_add - add two numbers
+ * @a: first num
+ * @b: second num
  *
- * Return: sum a & b
+ * Return: sum of a&b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - sub two num
- * @a: first number
- * @b: second number
+ * op_sub - sub two numbers
+ * @a: first num
+ * @b: second num
  *
- * Return: sum a & b
+ * Return: dif a & b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - mul two num
- * @a: first number
- * @b: second number
+ * op_mul - multi two num
+ * @a: first num
+ * @b: second num
  *
  * Return: sum a & b
  */
@@ -39,25 +35,37 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - div two num
- * @a: first number.
- * @b: second number.
+ * op_div - div two numbers
+ * @a: first number
+ * @b: second number
  *
- * Return: The quotient of a and b.
+ * Return: sum a & b
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
  * op_mod - remaind two numbers
- * @a: first number.
- * @b: second number.
+ * @a: first num
+ * @b: second num
  *
  * Return: sum a & b
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
