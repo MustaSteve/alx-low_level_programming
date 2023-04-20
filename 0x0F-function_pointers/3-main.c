48acf7b7342912159b589cc2a0a8f9138c830892
MustaSteve <avineusq@gmail.com>
2023-04-19 19:01:24 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,43 +1,40 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - do the code 
+ * main - do code
  * @argc: int
- * @argv: array 
- *
- * Return: 0 
+ * @argv: arrays
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-    int arl1, arl2, result;
-    int (*op_func)(int, int);
+	int in1, in2, result;
+	int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    arl1 = atoi(argv[1]);
-    arl2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    if (!op_func)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	op_func = get_op_func(argv[2]);
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && arl2 == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    result = op_func(arl1, arl2);
-    printf("%d\n", result);
+	in1 = atoi(argv[1]);
+	in2 = atoi(argv[3]);
+	result = op_func(in1, in2);
 
-    return (0);
+	printf("%d\n", result);
+	return (0);
 }
