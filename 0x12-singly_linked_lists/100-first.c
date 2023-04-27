7b219d16246dc045b7da2ee56eb8eecdea6af7a5
MustaSteve <avineusq@gmail.com>
2023-04-27 15:38:48 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * premain - pre main
+ */
+void __attribute__((constructor)) hare(void);
+/**
+ * hare -  a function that prints a string
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
